1c0a785d91dbee17d8a76364cb042c4c7c713035
Robbie Zhuang <robbie.zhuang@gmail.com>
2022-08-07 03:41:42 -0400

    Add chewtle button

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -11,8 +11,6 @@ async function searchCards(cardName: string) {
         return [];
     }
     const data = await response.json();
-    console.log('response', response);
-    console.log('No Error', data);
     return data.data.map((card) => {
         return {
             cardId: card.id,
@@ -37,15 +35,25 @@ function CardThumbnailDragged(cardImg, width, height): Element {
     div.appendChild(img);
     div.style.position = 'absolute';
     div.style.top = '0px';
-    div.style.left = '-10000px';
+    div.style.left = '-1000000px';
 
     document.querySelector('body').appendChild(div);
 
     return div;
 }
 
-const CardThumbnail = ({card, canvasZoomLevel, canvasCardWidth, canvasCardHeight, cardsLoaded, setCardsLoaded}) => {
+const CardThumbnail = ({
+    card,
+    canvasZoomLevel,
+    canvasCardWidth,
+    canvasCardHeight,
+    cardsLoaded,
+    setCardsLoaded,
+    droppedInIFrame,
+    setDroppedInIFrame,
+}) => {
     const [loadedSrc, setLoadedSrc] = React.useState(null);
+
     React.useEffect(() => {
         const fetchImage = async () => {
             const imageObjectUrl = await getCardImage(card.smallUrl);
@@ -73,12 +81,16 @@ const CardThumbnail = ({card, canvasZoomLevel, canvasCardWidth, canvasCardHeight
     };
 
     const onDragStart = async (e) => {
+        setDroppedInIFrame(false);
         const width = canvasCardWidth * canvasZoomLevel;
         const height = canvasCardHeight * canvasZoomLevel;
         e.dataTransfer.setDragImage(CardThumbnailDragged(loadedSrc, width, height), width / 2, height / 2);
     };
 
     const onDragEnd = async (e) => {
+        if (droppedInIFrame) {
+            return;
+        }
         window.parent.postMessage(
             {
                 pluginMessage: {
@@ -97,18 +109,25 @@ const CardThumbnail = ({card, canvasZoomLevel, canvasCardWidth, canvasCardHeight
 };
 
 const App = ({}) => {
-    let [cardInfos, setCardInfos] = React.useState([]);
-    let [cardsLoaded, setCardsLoaded] = React.useState(0);
+    const [cardInfos, setCardInfos] = React.useState([]);
+    const [cardsLoaded, setCardsLoaded] = React.useState(0);
     const [canvasZoomLevel, setCanvasZoomLevel] = React.useState(0);
     const [canvasCardWidth, setCanvasCardWidth] = React.useState(0);
     const [canvasCardHeight, setCanvasCardHeight] = React.useState(0);
     const [statusMessage, setStatusMessage] = React.useState('');
+    const [droppedInIFrame, setDroppedInIFrame] = React.useState(false);
+    const [showChewtle, setShowChewtle] = React.useState(false);
+
+    const windowRef = React.useRef();
 
     const [query, setQuery] = React.useState('');
 
     const enterSearch = async (k) => {
         if (k.keyCode === 13) {
-            const results = await searchCards(query);
+            var results = [];
+            if (query.trim().length !== 0) {
+                results = await searchCards(query.trim());
+            }
             setCardInfos(() => results);
             setCardsLoaded(0);
         }
@@ -133,8 +152,14 @@ const App = ({}) => {
     }, [cardInfos, cardsLoaded]);
 
     React.useEffect(() => {
-        setStatusMessage("Drag'n'Drop a card to the canvas to add it to the deck");
+        setStatusMessage("Drag'n'Drop a card to the canvas");
     }, []);
+
+    const onClick = (e) => {
+        if (e.detail === 5) {
+            setShowChewtle(true);
+        }
+    };
 
     const onMouseEnter = () => {
         // Get current zoom level on the screen
@@ -143,7 +168,18 @@ const App = ({}) => {
 
     // todo: make chewtle button more awesome
     return (
-        <div className="window" onMouseEnter={onMouseEnter}>
+        <div
+            onDragOver={(e) => {
+                e.dataTransfer.dropEffect = 'move';
+                e.preventDefault();
+            }}
+            onDrop={() => {
+                setDroppedInIFrame(true);
+            }}
+            className="window"
+            ref={windowRef}
+            onMouseEnter={onMouseEnter}
+        >
             <input
                 autoFocus
                 className="searchInput"
@@ -152,10 +188,11 @@ const App = ({}) => {
                 placeholder="Search card names"
                 onChange={(e) => setQuery(e.target.value)}
                 onKeyUp={enterSearch}
+                onClick={onClick}
             ></input>
-            <div id="searchStatus">
+            {/* <div id="searchStatus">
                 <label>{statusMessage}</label>
-            </div>
+            </div> */}
             <div id="results">
                 {cardInfos.map((card) => (
                     <React.Fragment key={card.cardId}>
@@ -166,14 +203,14 @@ const App = ({}) => {
                             canvasCardHeight={canvasCardHeight}
                             cardsLoaded={cardsLoaded}
                             setCardsLoaded={setCardsLoaded}
+                            droppedInIFrame={droppedInIFrame}
+                            setDroppedInIFrame={setDroppedInIFrame}
                         />
                     </React.Fragment>
                 ))}
             </div>
-            <div id="chewtleDiv">
-                <button id="insertChewtle" onClick={insertChewtle}>
-                    Insert Grass Energy
-                </button>
+            <div style={{display: `${showChewtle ? 'block' : 'none'}`}} id="chewtleDiv">
+                <button id="insertChewtle" onClick={insertChewtle} />
             </div>
         </div>
     );
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -4,46 +4,35 @@ const cardHeight = 880;
 figma.showUI(__html__);
 figma.ui.resize(500, 500);
 
+function addCardToCanvas(msg, x, y) {
+    const image = figma.createImage(new Uint8Array(msg.data));
+    const newPaint = {
+        type: 'IMAGE',
+        scaleMode: 'FIT',
+        imageHash: image.hash,
+    };
+    const shape = figma.createRectangle();
+    shape.resize(cardWidth, cardHeight);
+    shape.x = x;
+    shape.y = y;
+    const newFills = [];
+    newFills.push(newPaint);
+    shape.fills = newFills;
+
+    figma.currentPage.appendChild(shape);
+
+    figma.currentPage.selection = [shape];
+}
+
 figma.ui.onmessage = (msg) => {
     if (msg.type === 'card') {
-        console.log('connection!');
-
-        const image = figma.createImage(new Uint8Array(msg.data));
-        const newPaint = {
-            type: 'IMAGE',
-            scaleMode: 'FIT',
-            imageHash: image.hash,
-        };
-        const shape = figma.createRectangle();
-        shape.resize(cardWidth, cardHeight);
-        shape.x = figma.viewport.center.x - cardWidth / 2;
-        shape.y = figma.viewport.center.y - cardHeight / 2;
-        const newFills = [];
-        newFills.push(newPaint);
-        shape.fills = newFills;
-
-        figma.currentPage.appendChild(shape);
-
-        figma.currentPage.selection = [shape];
+        addCardToCanvas(msg, figma.viewport.center.x - cardWidth / 2, figma.viewport.center.y - cardHeight / 2);
     } else if (msg.type === 'cardDrag') {
-        const image = figma.createImage(new Uint8Array(msg.data));
-        const newPaint = {
-            type: 'IMAGE',
-            scaleMode: 'FIT',
-            imageHash: image.hash,
-        };
-        const shape = figma.createRectangle();
-        shape.resize(cardWidth, cardHeight);
-
-        shape.x = figma.viewport.bounds.x + msg.pos[0] / figma.viewport.zoom - cardWidth / 2;
-        shape.y = figma.viewport.bounds.y + (msg.pos[1] - 48) / figma.viewport.zoom - cardHeight / 2;
-        const newFills = [];
-        newFills.push(newPaint);
-        shape.fills = newFills;
-
-        figma.currentPage.appendChild(shape);
-
-        figma.currentPage.selection = [shape];
+        addCardToCanvas(
+            msg,
+            figma.viewport.bounds.x + msg.pos[0] / figma.viewport.zoom - cardWidth / 2,
+            figma.viewport.bounds.y + (msg.pos[1] - 48) / figma.viewport.zoom - cardHeight / 2
+        );
     } else if (msg.type === 'zoomRequest') {
         figma.ui.postMessage({
             zoom: figma.viewport.zoom,
